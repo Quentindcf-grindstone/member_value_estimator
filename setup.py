86a256518307ba43e3bd4a8400952d3eb0dc9c26
Quentindcf-grindstone <quentin.dechauffour@grindstonecapital.co.uk>
2021-04-26 10:07:15 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A lightweight estimator of the total lifetime revenue of a member. This will be used to estimate the value of recent traffic from certain affiliates, countries and bin ranges. ',
+    author='Quentin Dechauffour - Grindstone Capital',
+    license='MIT',
+)
---
+++ src/data/make_dataset.py
@@ -0,0 +1,10 @@
+from pydomo import Domo
+import pandas as pd
+from dotenv import dotenv_values
+config = dotenv_values(".env")
+
+domo = Domo(config['domo_client_id'], config['domo_secret'], api_host='api.domo.com')
+
+member_data = domo.ds_get('82b72ffd-008e-460e-ae34-fe69d988197e')
+member_data.to_csv('../data/raw/member_data_raw.csv')
+
